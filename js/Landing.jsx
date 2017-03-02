fddd84a97bb387420b6a3479776e8584ed04e7e5
Mo Amini <mammutit89@gmail.com>
2017-03-02 06:42:41 +0100

    added aws logo

--- js/Landing.jsx
+++ js/Landing.jsx
@@ -5,17 +5,22 @@ var FontAwesome = require('react-fontawesome');
 
 
 const Landing = () => (
-  <div className="container background">
-    <div className="row">
-      <div className="col-xs-12 col-md-12 col-lg-12 col-xl-12 text-center">
-        <span className='heading'>SOMETHING SPECTACULAR IS ABOUT TO HAPPEN</span>
-        <FontAwesome
-          name='cloud'
-          size='5x'
-          spin
-          style={{ textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)' }}
-        />
+  <div>
+    <div className="container background">
+      <div className="row">
+        <div className="col-xs-12 col-md-12 col-lg-12 col-xl-12 text-center">
+          <span className='heading'>SOMETHING SPECTACULAR IS ABOUT TO HAPPEN</span>
+          <FontAwesome
+            name='cloud'
+            size='5x'
+            spin
+            style={{ textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)' }}
+          />
+        </div>
       </div>
+    </div>
+    <div id="footer">
+      <img src="public/images/AWS_logo_poweredby_black_127px.png"/>
     </div>
   </div>
 )
